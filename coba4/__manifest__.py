cca75e07f1c64c0c66bc37a70ec3440c886c0996
raihani.rey <h.j.ujan@gmail.com>
2024-06-23 09:02:54 +0700

    initial commit

---
+++ coba4/__manifest__.py
@@ -0,0 +1,28 @@
+{
+   "name":  "water lily",
+   "version":   "2.0",
+   "website":   "https://www.odoo.com/app/project",
+   "category":  "Service/view menu",
+   "author":    "₊˚ reii ⊹♡",
+   "sequence":  20,
+   "description": '''
+        This is the very new website created by ₊˚ reii ⊹♡
+        as her assignment to be a good newbie programer.
+    ''',
+   "summary":   "make your own website :)", 
+   "depends":   [
+        "point_of_sale",
+        "base_setup",
+        "resource",
+        "mail",
+        "web",
+        "web_tour",
+        "digest",
+        "rating",
+   ],
+    "images": [
+        "static/description/banner.png",
+        "static/theme_screenshot.png"
+    ],
+    
+}
