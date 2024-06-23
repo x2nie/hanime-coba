f8fb47e2ff22c33c4b7bfd90295c81140960bf46
raihani.rey <h.j.ujan@gmail.com>
2024-06-23 09:40:25 +0700

    tambahin author dan license

--- waterlily/__manifest__.py
+++ waterlily/__manifest__.py
@@ -3,6 +3,8 @@
    "version":   "2.0",
    "category":  "Service/view menu",
    "sequence":  20,
+   "author":    "₊˚ reii ⊹♡",
+   "license":   "LGPL-3",
    "summary":   "my experimental module :)", 
    "depends":   [
         "point_of_sale",
