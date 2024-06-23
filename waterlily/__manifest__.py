2933f2ccae1ff98383250155560a7784d99d8535
raihani.rey <h.j.ujan@gmail.com>
2024-06-23 09:33:29 +0700

    modul baru

---
+++ waterlily/__manifest__.py
@@ -0,0 +1,19 @@
+{
+   "name":  "water lily",
+   "version":   "2.0",
+   "website":   "https://www.odoo.com/app/project",
+   "category":  "Service/view menu",
+   "sequence":  20,
+   "summary":   "make your own website :)", 
+   "depends":   [
+        "point_of_sale",
+        "base_setup",
+        "resource",
+        "mail",
+        "web",
+        "web_tour",
+        "digest",
+        "rating",
+   ],
+    
+}
